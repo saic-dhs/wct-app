7fcecbf874f7736ff6a72f23215d9dbd9648029e
Andrew Roth <roth.andy@gmail.com>
2019-08-28 22:18:52 -0400

    Add files from 1.2.3 folder

--- wct_app/lib/widgetizer/widgetizer.py
+++ wct_app/lib/widgetizer/widgetizer.py
@@ -1,6 +1,6 @@
 from wct_app.config.wct_config import WCT_SECRET_SAUCE, PATH_TO_DATA_FILE
 
-class NewWidgetizer:
+class Widgetizer:
     def __init__(self, widget_material):
         self.widget_material = str(widget_material)
         self.secret_sauce = int(WCT_SECRET_SAUCE)
@@ -8,18 +8,6 @@ class NewWidgetizer:
         for _ in range(self.secret_sauce)])
 
     def save_widget(self):
+
         with open('{}/widget.store'.format(PATH_TO_DATA_FILE), 'a') as outfile:
             outfile.write('\n\n{}'.format(self.widget))
-
-    def foo(self):
-        var12 = var5(arg2, arg1)
-        if arg1 < arg2:
-            var17 = class5()
-        else:
-            var17 = class7()
-        for var18 in range(15):
-            var19 = var17.func6
-            var19(var18, arg2)
-
-    def bar(self, arg15, arg16):
-        return 0
--- wct_app/tests/wct_tests.py
+++ wct_app/tests/wct_tests.py
@@ -12,6 +12,15 @@ def client():
 
 ROUTE = '/api/v0/widgetize'
 
+def test_env_var_exist():
+    assert os.getenv('SECRET_SAUCE') != None
+    assert os.getenv('WCT_VERSION') != None
+    assert os.getenv('PATH_TO_DATA_FILE') != None
+
+def test_no_payload(client):
+    r = client.post(ROUTE)
+    assert r.status_code == 400
+
 def test_good_payload(client):
     r = client.post(ROUTE, data=json.dumps({'widget_material': 'foo'}))
     data = r.get_json()
